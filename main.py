fb88422edae924d806d52a79ee9a0e77b2816430
Enrico-Programador <enrico2015ap@gmail.com>
2025-04-02 10:28:26 -0300

    Version 1.0.0

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import sys
 from stats import get_book_text, get_num_chars, get_num_words, word_count
 
 def main():
-
-    text = get_book_text("books/frankenstein.txt")
+     
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+        
+    text = get_book_text(sys.argv[1])
     count = get_num_chars(text)
 
     print("============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...\n----------- Word Count ----------")
