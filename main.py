70ab1401c7fa3bc8bc250445d31433e27c5a2d7a
Enrico-Programador <enrico2015ap@gmail.com>
2025-04-02 10:34:57 -0300

    Version 1.0.1

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ def main():
     if len(sys.argv) != 2:
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
-        
+
     text = get_book_text(sys.argv[1])
     count = get_num_chars(text)
 
-    print("============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...\n----------- Word Count ----------")
+    print(f"============ BOOKBOT ============\nAnalyzing book found at {sys.argv[1]}...\n----------- Word Count ----------")
     get_num_words(text)
     print("--------- Character Count -------")
 
